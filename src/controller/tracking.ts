620fa140d51c23d7a1f4196acf05db14c2a4ba75
LibnyAzul <libnyazul@gmail.com>
2024-03-18 18:01:10 -0600

    Se agregan filtros para tracking(filtros por fecha)

--- src/controller/tracking.ts
+++ src/controller/tracking.ts
@@ -44,3 +44,80 @@ export const getTracking: RequestHandler = async (req, res) => {
   return res.status(200).json({ trackingFaund, message: "OK" });
 };
 
+export const trackingByDate: RequestHandler = async (req, res) => {
+  try {
+    const vehicleId = req.params.vehicleId;
+    const { objectsPerPage, page, filters } = req.body;
+
+    const vehicle = await Vehicle.findById(vehicleId);
+
+    if (!vehicle) {
+      return res.status(404).json({ message: "Vehicle not found" });
+    }
+
+    let query: any = { _id: { $in: vehicle.tracking } };
+    if (filters && filters.length > 0) {
+      filters.forEach((filter: any) => {
+        switch (filter.filter) {
+          case "equals":
+            const equalDate = new Date(filter.value);
+            equalDate.setUTCHours(0, 0, 0, 0);
+            console.log(equalDate);
+            const endOfDay = new Date(equalDate);
+            endOfDay.setUTCHours(23, 59, 59, 999);
+            query[filter.field] = {
+              $gte: equalDate,
+              $lte: endOfDay,
+            };
+            break;
+          case "between":
+            const [startDate, endDate] = filter.value.split(",");
+            const startOfDay = new Date(startDate);
+            startOfDay.setUTCHours(0, 0, 0, 0);
+            const endDay = new Date(endDate);
+            endDay.setUTCHours(23, 59, 59, 999);
+            query[filter.field] = {
+              $gte: startOfDay,
+              $lte: endDay,
+            };
+            break;
+          default:
+            // Manejar filtro no reconocido
+            break;
+        }
+      });
+    }
+
+    // Contar total de registros a partir de los filtros
+    const total = await Tracking.countDocuments(query);
+
+    // Aplicar paginación
+    const skip = (page - 1) * objectsPerPage;
+
+    // Se aplica la paginación
+    const trackings: ITrackingDocument[] = await Tracking.find(query)
+      .skip(skip)
+      .limit(objectsPerPage);
+
+    // Calcular información de paginación
+    const maxPage = Math.ceil(total / objectsPerPage);
+    const previousPage = page > 1 ? page - 1 : 1;
+    const nextPage = page < maxPage ? page + 1 : maxPage;
+
+    return res.json({
+      objectsPerPage,
+      page,
+      maxPage,
+      previousPage,
+      nextPage,
+      total,
+      filters,
+      list: trackings,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: "Error searching trackings by date",
+      error: error.message,
+    });
+  }
+};
--- src/routes/tracking.ts
+++ src/routes/tracking.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.post("/tracking", trackingCtr.createTracking);
 router.delete("/tracking/:id", trackingCtr.deleteTracking);
 router.get("/tracking/:id", trackingCtr.getTracking);
+router.post("/tracking/vehicle/:vehicleId", trackingCtr.trackingByDate);
 
 export default router;
